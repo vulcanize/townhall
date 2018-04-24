a21d7b6cf4f98912830722d39a30f6abfb3cd6c2
gustin <gustin@entryway.net>
2018-04-24 08:34:06 -0400

    Use pm2 ecosystem for deployment.

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'Menlo Town Hall',
+      script    : 'yarn',
+      args      : 'run build-run:staging',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy : {
+    staging : {
+      user : 'deployer',
+      host : 'ec2-13-58-70-6.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/persistent-contract-addresses',
+      repo : 'https://github.com/vulcanize/townhall.git',
+      path : '/var/www/menlo-town-hall',
+      key  : '../menlo-infra/menlo-staging-deploy.pem',
+      ssh_options: ['ForwardAgent=yes', 'StrictHostKeyChecking=no','PasswordAuthentication=no'],
+      'post-deploy' : 'yarn && pm2 reload ecosystem.config.js --env staging'
+    }
+  }
+};
